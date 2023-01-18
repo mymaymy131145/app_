446c5bdbbde6fcee210195bcbe9bd46941f39ad4
arapan may <mymaymy131145@gmail.com>
2023-01-19 01:19:23 +0700

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts
+    endvalidates FILL_IN, presence: true # Replace FILL_IN with the
+right code.
+    validates FILL_IN, presence: true # Replace FILL_IN with the
+right code.
 end
